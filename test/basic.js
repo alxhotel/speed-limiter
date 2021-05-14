fa6084e6f02d9a4ca52f73d3810df77362e922b8
Alex <alxmorais8@msn.com>
2021-05-14 15:28:16 +0200

    Fix for rate = -1

--- test/basic.js
+++ test/basic.js
@@ -12,7 +12,7 @@ const dataToSend = (() => {
 })()
 
 const opts = {
-  rate: 10 * 1000 // 100 KB/s
+  rate: 10 * 1000 // 10 KB/s
 }
 
 function testThrottle (throttle, cb) {
@@ -22,8 +22,12 @@ function testThrottle (throttle, cb) {
   })
   throttle.on('end', () => {
     const isEqual = (dataToSend === dataReceived)
-    cb(isEqual)
+    const after = Date.now()
+    const speed = dataToSend.length / ((after - before) / 1000)
+    cb(isEqual, speed)
   })
+
+  const before = Date.now()
   throttle.write(dataToSend, () => {
     throttle.end()
   })
@@ -36,6 +40,8 @@ test('Throttle must send the data', (t) => {
 
   testThrottle(throttle, (ok) => {
     t.ok(ok, 'received string should equal sent string')
+
+    throttle.destroy()
   })
 })
 
@@ -45,8 +51,12 @@ test('Throttle from ThrottleGroup must send the data', (t) => {
   const group = new ThrottleGroup(opts)
 
   async.each([1, 2, 3], () => {
+    const throttle = group.throttle()
+
     testThrottle(group.throttle(), (ok) => {
       t.ok(ok, 'received string should equal sent string')
+
+      throttle.destroy()
     })
   }, (err) => {
     if (err) return t.error(err)
@@ -58,12 +68,59 @@ test('Speed must be lower than the rate set', (t) => {
 
   const throttle = new Throttle(opts)
 
-  const before = Date.now()
-  testThrottle(throttle, () => {
-    const after = Date.now()
-    const speed = dataToSend.length / ((after - before) / 1000)
-
+  testThrottle(throttle, (_, speed) => {
     const lessThanRate = (speed <= opts.rate)
     t.ok(lessThanRate, 'speed should be less or equal to rate')
+
+    throttle.destroy()
   })
+})
+
+test('Speed must be higher than 10 KB/s, when disabled and rate = 0', (t) => {
+  t.plan(1)
+
+  const throttle = new Throttle({
+    rate: 0,
+    enabled: false
+  })
+
+  testThrottle(throttle, (_, speed) => {
+    const lessThanRate = (speed >= 10 * 1000)
+    t.ok(lessThanRate, 'speed should be more than 10 KB/s')
+
+    throttle.destroy()
+  })
+})
+
+test('Speed must be higher than 10 KB/s, when disabled and rate is > 0', (t) => {
+  t.plan(1)
+
+  const throttle = new Throttle({
+    rate: 100,
+    enabled: false
+  })
+
+  testThrottle(throttle, (_, speed) => {
+    const lessThanRate = (speed >= 10 * 1000)
+    t.ok(lessThanRate, 'speed should be more than 10 KB/s')
+
+    throttle.destroy()
+  })
+})
+
+test('Throttle should block everything if rate is zero', (t) => {
+  const throttle = new Throttle({
+    rate: 0,
+    enabled: true
+  })
+
+  testThrottle(throttle, () => {
+    t.fail('throttle has not block all chunks')
+  })
+
+  setTimeout(() => {
+    throttle.destroy()
+    t.pass('throttle has block all chunks')
+    t.end()
+  }, 2000)
 })
